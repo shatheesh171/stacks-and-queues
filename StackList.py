f8e4b08b9c0c355f9aa678f894021a120034c309
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-10-25 13:53:33 +0530

    stack python list

--- StackList.py
+++ StackList.py
@@ -4,6 +4,50 @@ class Stack:
         self.list=[]
 
     def __str__(self):
-        values=self.list.reverse()
-        values=[str(x) for x in self.list]
+        if len(self.list):
+            print(self.list)
+            return
+        values=reversed(self.list)
+        values=[str(x) for x in values]
         return "\n".join(values)
+
+    #isEmpty
+    def isEmpty(self):
+        if self.list==[]:
+            return True
+        else:
+            return False
+
+    #Push
+    def push(self,value):
+        self.list.append(value)
+        return "Element inserted in stack successfully"
+
+    #Pop
+    def pop(self):
+        #print(self.list)
+        if self.isEmpty():
+            return "No elements in the stack"
+        return self.list.pop()
+
+    #Peek
+    def peek(self):
+        if self.isEmpty():
+            return "No elements in the stack"
+        return self.list[len(self.list)-1]
+
+    #delete complete stack
+    def delete(self):
+        self.list=None
+
+#Custom Stack
+cs=Stack()
+print(cs.isEmpty())
+cs.push(1)
+cs.push(2)
+cs.push(3)
+#print(cs)
+cs.pop()
+cs.delete()
+print(cs)
+print(cs.peek())
---
+++ temp.py
@@ -0,0 +1,9 @@
+a=[1,2,3]
+a.append(4)
+a.append(5)
+print(a)
+a.pop()
+a.pop()
+a.pop()
+print(reversed(a))
+print(a)
